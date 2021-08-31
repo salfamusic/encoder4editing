5f4602fedfcf90bbee3169d71a4acd4f9b9ce3af
Michael King <salfa.music@gmail.com>
2021-08-31 17:06:07 -0500

    moving to cuda

--- module/styleflow.py
+++ module/styleflow.py
@@ -16,4 +16,4 @@ def get_styleflow_model():
     prior.load_state_dict(torch.load('flow_weight/modellarge10k.pt'))
     prior.eval()
 
-    return session, model, w_avg, prior.cpu()
+    return session, model, w_avg, prior.cuda()
